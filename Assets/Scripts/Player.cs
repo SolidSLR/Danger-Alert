041154fe2d07ba73a8b290cbb7525d2088ef47a5
Santiago López Rey <devsantilopez@gmail.com>
2023-03-01 20:35:29 +0100

    Salto para Player y física básica

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,21 +6,49 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float speed;
+    public float jumpForce;
+    private bool isJumping = false;
+    private Vector3 newPosition;
+    Vector3 velocity;
     // Start is called before the first frame update
     void Start()
     {
-     rb = gameObject.GetComponent<Rigidbody2D>();  
+     rb = gameObject.GetComponent<Rigidbody2D>();
+     newPosition = new Vector3();
+     velocity = new Vector3();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xPostition = rb.transform.position.x;
-        if(Input.GetKey(KeyCode.D)){
-            transform.position += Vector3.right * speed * Time.deltaTime;
+        Vector3 prevPos = transform.position;
+        Debug.Log("Posición previa: "+prevPos);        
+        if(!isJumping){
+            if(Input.GetKey(KeyCode.D)){
+                newPosition = Vector3.right * speed * Time.deltaTime;
+                rb.transform.position += newPosition;
+            }
+            if(Input.GetKey(KeyCode.A)){
+                newPosition = Vector3.left * speed * Time.deltaTime;
+                rb.transform.position += newPosition;
+            }
+            if(Input.GetKeyDown(KeyCode.Space)){
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
         }
-        if(Input.GetKey(KeyCode.A)){
-            transform.position += Vector3.left * speed * Time.deltaTime;
+        Debug.Log("Posición actual: "+transform.position);
+        
+    }
+
+    void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.tag == "Ground"){
+            isJumping = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.tag == "Ground"){
+            isJumping = false;
         }
     }
 }
