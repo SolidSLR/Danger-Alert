bdfae761300e574f07efd59b71ec3733f56cf874
Santiago López Rey <devsantilopez@gmail.com>
2023-03-01 19:47:34 +0100

    Movimiento básico de Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+     rb = gameObject.GetComponent<Rigidbody2D>();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xPostition = rb.transform.position.x;
+        if(Input.GetKey(KeyCode.D)){
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
     }
 }
